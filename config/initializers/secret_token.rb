b6e03aa5ac39518ac3775722d8ab093be2ad03f2
Hoa Hoang <nobi.hoa@gmail.com>
2014-07-31 21:45:42 +0700

    Initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SiliconStore::Application.config.secret_key_base = 'e68822f3eef0df922b9cf630d13b45147f4359f9763a963fb62cf63f36efc8a05b803d1773ac0685c13614af0bd400c3e38880999ccf2e85ce3aad6692b09551'

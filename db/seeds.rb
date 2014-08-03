1166c18051a55e6c42941b4aead4d1a0fb8b5b5b
Hoa Hoang <nobi.hoa@gmail.com>
2014-08-03 10:37:12 +0700

    Add database seed file for generating sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,23 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+if User.count == 0
+  admin = User.create(first_name: "Super", last_name: "Admin", email: "admin@example.com", date_of_birth: "1990-03-09", password: "12345678")
+  admin.add_role :admin
+
+  users = User.create([
+    { first_name: "John", last_name: "Smith", email: "user@example.com", date_of_birth: "1989-04-01", password: "12345678" },
+    { first_name: "Britney", last_name: "Spear", email: "member@example.com", date_of_birth: "1989-04-01", password: "12345678" }
+  ])
+end
+
+if Vendor.count == 0
+  Vendor.create([
+    { name: "Apple", description: "Apple Inc. is an American multinational corporation headquartered in Cupertino, California"},
+    { name: "Samsung", description: "Samsung Inc. is a South Korean multinational conglomerate company headquartered in Samsung Town, Seoul" }
+  ])
+end
+
+if Category.count == 0
+  Category.create([
+    { name: "Tablet" },
+    { name: "Phone" }
+  ])
+end

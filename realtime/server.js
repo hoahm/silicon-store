69a58d3c55a93cb3b2ae3e5fd3a3d721f214da0e
Hoa Hoang <nobi.hoa@gmail.com>
2014-08-07 21:48:40 +0700

    Edit NodeJS package description

--- realtime/server.js
+++ realtime/server.js
@@ -2,17 +2,21 @@ process.title = 'silicon-store-server';
 
 var express = require('express');
 var app = express();
-
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-
 var redis = require("redis");
+var port = process.env.PORT || 5000;
 
 // Redis URL from Heroku
 var url = require('url');
-var redisURL = url.parse("redis://rediscloud:021oGegbXNw8Dkqc@pub-redis-19375.us-east-1-4.1.ec2.garantiadata.com:19375");
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
 
-server.listen(3001);
+io.configure(function () {
+  io.set("transports", ["xhr-polling"]);
+  io.set("polling duration", 12);
+});
+
+server.listen(port);
 
 // simple logger
 app.use(function(req, res, next){
@@ -29,14 +33,11 @@ io.sockets.on('connection', function (socket) {
 
   // relay redis messages to connected socket
   subscribe.on("message", function(channel, message) {
-    console.log("from rails to subscriber:", channel, message);
-    socket.emit('message', message)
+    socket.emit('message', message);
   });
 
   // unsubscribe from redis if session disconnects
   socket.on('disconnect', function () {
-    console.log("user disconnected");
-
     subscribe.quit();
   });
 
